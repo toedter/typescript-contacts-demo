b1cdec878fe2b2551ed7f7e2f4760ab5f6bade11
Kai Tödter <kai@toedter.com>
2013-10-07 09:01:51 +0200

    better disclaimers

--- src/main/ts/contact.ts
+++ src/main/ts/contact.ts
@@ -1,7 +1,7 @@
 /*
  TypeScript Contacts Demo
 
- Copyright [yyyy] [name of copyright owner]
+ Copyright 2013 Kai Toedter
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
-*/
+ */
 
 module cdemo {
     export interface Contact {
