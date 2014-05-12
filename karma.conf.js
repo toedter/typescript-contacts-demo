4e0078e21e7dd321a9f74aa0a8649bc644caf6fe
Kai Tödter <kai@toedter.com>
2014-05-12 17:01:44 +0200

    added karma code coverage => 100% :)

--- karma.conf.js
+++ karma.conf.js
@@ -25,10 +25,13 @@ module.exports = function (config) {
 
         ],
 
+        preprocessors: {
+            'src/**/*.js': ['coverage']
+        },
 
         // test results reporter to use
         // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
 
 
         // web server port
--- src/test/ts/eventSpec.ts
+++ src/test/ts/eventSpec.ts
@@ -27,17 +27,18 @@ describe('Event', () => {
         var event:cdemo.Event = new cdemo.Event(cdemo.EventType.SUBSCRIBE, 'id-1', contact);
         expect(event.getSourceId()).toBe('id-1');
         expect(event.getType()).toBe(cdemo.EventType.SUBSCRIBE);
+        expect(event.getData()).toBe(contact);
     });
 
     it('should be created from valid json', () => {
-        var json = {"type":"UNSUBSCRIBE","sourceId":"id-2","contact":{"firstName":"Kai","lastName":"Toedter","email":"kai@toedter.com"}};
+        var json = {"type": "UNSUBSCRIBE", "sourceId": "id-2", "contact": {"firstName": "Kai", "lastName": "Toedter", "email": "kai@toedter.com"}};
         var event:cdemo.Event = cdemo.Event.fromJson(JSON.stringify(json));
         expect(event.getSourceId()).toBe('id-2');
         expect(event.getType()).toBe(cdemo.EventType.UNSUBSCRIBE);
     });
 
     it('should create UNKNOWN event type from valid json event type', () => {
-        var json = {"type":"xxx","sourceId":"id-2","contact":{"firstName":"Kai","lastName":"Toedter","email":"kai@toedter.com"}};
+        var json = {"type": "xxx", "sourceId": "id-2", "contact": {"firstName": "Kai", "lastName": "Toedter", "email": "kai@toedter.com"}};
         var event:cdemo.Event = cdemo.Event.fromJson(JSON.stringify(json));
         expect(event.getSourceId()).toBe('id-2');
         expect(event.getType()).toBe(cdemo.EventType.UNKNOWN);
